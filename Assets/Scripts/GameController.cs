e7d0ef95a30691b2f11eca9b7498e740b018a343
annalisegong <annalise.gong@cuw.edu>
2022-11-02 20:19:27 -0500

    added onClick functions
    x and o can now be placed in the desired spot in the grid

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,8 +9,9 @@ public class GameController : MonoBehaviour
     public int whoseTurn; //0 = x and 1 = o
     public int turnCount; // count nunber of turns played
     public GameObject[] turnIcons; //displays whose turn it is
-    public Sprite[] playIcons; // 0 = x and 1 = y
+    public Sprite[] playerIcons; // 0 = x and 1 = o
     public Button[] ticTacToeSpaces; // playable spaces
+    public int[] markedSpaces; // IDs which space was marked by which player
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +31,42 @@ public class GameController : MonoBehaviour
             ticTacToeSpaces[i].interactable =true;
             ticTacToeSpaces[i].GetComponent<Image>().sprite = null;
         }
+        for(int i = 0; i < markedSpaces.Length; i++)
+        {
+            markedSpaces[i] = -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ticTacToeButton(int whichNumber)
+    {
+        //places x or o in clicked button
+        ticTacToeSpaces[whichNumber].image.sprite = playerIcons[whoseTurn];
+        //button cannot change once clicked
+        ticTacToeSpaces[whichNumber].interactable = false;
+
+        //IDs which space is marked by which player x = 0 and o = 1;
+        markedSpaces[whichNumber] = whoseTurn;
+        turnCount++;
+
+        if(whoseTurn == 0)
+        {
+            whoseTurn = 1;
+            //the following lines display whose turn via the arrows
+            turnIcons[0].SetActive(false);
+            turnIcons[1].SetActive(true);
+        }
+        else
+        {
+            whoseTurn = 0;
+            //the following lines display whose turn via the arrows
+            turnIcons[0].SetActive(true);
+            turnIcons[1].SetActive(false);
+        }
     }
 }
